1d577e8d93e3784732a98b48f44917e3555593da
Jaciesune <lutasek9@gmail.com>
2024-10-28 18:44:34 +0100

    Carente | Reservations View For The User

---
+++ Controllers/RezerwacjeController1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using Carente.Models;
+
+namespace Carente.Controllers
+{
+    public class RezerwacjeController : Controller
+    {
+        private readonly CarenteContext _context;
+
+        public RezerwacjeController(CarenteContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+
+            var rezerwacje = _context.Rezerwacja
+                .Where(r => r.Uzytkownik_Id == userId)
+                .Join(_context.Samochod,
+                      rez => rez.Samochod_Id,
+                      car => car.Id,
+                      (rez, car) => new
+                      {
+                          rezerwacja = rez,
+                          car = car,
+                          offer = car.Oferta
+                      })
+                .Select(result => new RezerwacjaViewModel
+                {
+                    Samochod_Id = result.rezerwacja.Samochod_Id,
+                    Marka = result.car.Marka,
+                    Model = result.car.Model,
+                    Cena = (decimal)result.offer.Cena,
+                    Data_Rozpoczecia = result.rezerwacja.Data_Rozpoczecia,
+                    Data_Zakonczenia = result.rezerwacja.Data_Zakonczenia
+                })
+                .ToList();
+
+            return View(rezerwacje);
+        }
+    }
+}
---
+++ Models/RezerwacjaViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Carente.Models
+{
+    public class RezerwacjaViewModel
+    {
+        public int Samochod_Id { get; set; }
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public decimal Cena { get; set; }
+        public DateTime Data_Rozpoczecia { get; set; }
+        public DateTime Data_Zakonczenia { get; set; }
+    }
+}
