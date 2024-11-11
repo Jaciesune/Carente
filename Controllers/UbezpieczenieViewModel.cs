d31e89874813e982eb4744c2f599eeb082d83588
Jakub Piwko <jakub.piwko@op.pl>
2024-11-11 15:05:56 +0100

    Carente | Insurance View

---
+++ Controllers/UbezpieczenieViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Carente.Controllers
+{
+    internal class UbezpieczenieViewModel
+    {
+        public string Typ { get; set; }
+        public int Wartosc { get; set; }
+        public string Opis { get; set; }
+    }
+}
